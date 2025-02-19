dccaedd5c478e51745bf8bfc3c9d5b34372371ad
CarboneGhosn <ghosn.dani@hotmail.com>
2025-02-19 20:58:10 -0300

    more codes

---
+++ bytebank.Util/ListaDeContasCorrentes.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebank_ATENDIMENTO.bytebank.Util
+{
+	public class ListaDeContasCorrentes
+	{
+		private ContasCorrentes[] itens = null;
+        private int _proximaPosicao=0;
+
+        public ListaDeContasCorrentes(int tamanhoInicial=5)
+        {
+            _itens = new ContaCorrente[tamanhoInicial];
+        }
+
+        public void Adicionar(ContaCorrente item)
+        {
+            Console.WriteLine($"Adicionando item na posição{_proximaPosicao}"
+            _itens[_proximaPosicao] = item;
+            _proximaPosicao++;
+        }
+    }
+}
