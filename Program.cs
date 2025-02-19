766b662302d8479ade491ff849df707fe586f230
CarboneGhosn <ghosn.dani@hotmail.com>
2025-02-19 20:29:51 -0300

    more codes

--- Program.cs
+++ Program.cs
@@ -1,8 +1,11 @@
-﻿Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
+﻿using bytebank.Modelos.Conta;
 
-TestaArreyInt();
+Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
 
-Console.WriteLine("Pressione qualquer tecla para sair...");
+//TestaArreyInt();
+//TestaBuscarPalavras();
+
+
 Console.ReadKey(); 
 
 void TestaArreyInt()
@@ -28,4 +31,76 @@ void TestaArreyInt()
 
 }
 
+void TestaBuscarPalavras()
+{
+    string[] arrayDePalavras = new string[5];
 
+    for (int i = 0; i < arrayDePalavras.Length; i++)
+    {
+        Console.Write($"Digite {i + 1}ª Palavra:  ");
+        arrayDePalavras[i] = Console.ReadLine();
+    }
+
+    Console.Write("Digite palavra a ser encontrada: ");
+    var busca = Console.ReadLine();
+
+    foreach (string palavra in arrayDePalavras)
+    {
+        if (palavra.Equals(busca))
+        {
+            Console.WriteLine($"Palavra encontra = {busca}.");
+            break;
+        }
+
+    }
+
+}
+
+
+
+
+Array amostra = Array.CreateInstance(typeof(double), 5);
+amostra.SetValue(5.9, 0);
+amostra.SetValue(1.8, 1);
+amostra.SetValue(7.1, 2);
+amostra.SetValue(10, 3);
+amostra.SetValue(6.9, 4);
+
+//[5,9][1,8][7,1][10][6,9]
+//TestaMediana(amostra);
+
+void TestaMediana(Array array)
+{
+    if ((array == null) || (array.Length == 0))
+    {
+        Console.WriteLine("Array para cálculo da mediana está vazio ou nulo.");
+    }
+
+    double[] numerosOrdenados = (double[])array.Clone();
+    Array.Sort(numerosOrdenados);
+    //[1,8][5,9][6,9][7,1][10]
+
+    int tamanho = numerosOrdenados.Length;
+    int meio = tamanho / 2;
+    double mediana = (tamanho % 2 != 0) ? numerosOrdenados[meio] :
+                                                             (numerosOrdenados[meio] + numerosOrdenados[meio - 1]) / 2;
+    Console.WriteLine($"Com base na amostra a mediana = {mediana}");
+}
+
+void TestaArrayDeContasCorrentes()
+{
+    ContaCorrente[] listaDeContas = new ContaCorrente[]
+    {
+        new ContaCorrente(874, "5679787-A"),
+        new ContaCorrente(874, "4456668-B"),
+        new ContaCorrente(874, "7781438-C")
+    };
+
+    for (int i = 0; i < listaDeContas.Length; i++)
+    {
+        ContaCorrente contaAtual = listaDeContas[i];
+        Console.WriteLine($"Índice{i} - Conta:{contaAtual.Conta}");
+    };
+}
+
+TestaArrayDeContasCorrentes();
